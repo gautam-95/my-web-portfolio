6198d142378e376862bc75321f63509ab814d96e
Gautam Puranik <gautam.puranik95@gmail.com>
2024-08-07 18:17:59 +0530

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,12 +5,12 @@ const Home = (props) => {
   return (
     <div className={classes.home}>
       <h2 className={classes.greeting}>Hi, I'm</h2>
-      <h1 className={classes.title}>Gautam Puranik.</h1>
+      <h1 className={classes.title}>Gautamraj Puranik.</h1>
       <h2 className={classes.subTitle}>I build and develop for the web.</h2>
       <p className={classes.summary}>
-        Experienced and slightly JavaScript-obsessed developer with a knack for crafting awesome web experiences.
-        My expertise spans across React, Node.js, GraphQL, and the entire JavaScript ecosystem.
-        With a passion for clean and efficient solutions, I love turning complex problems into elegant applications.
+        Passionate JS developer with a knack of crafting awesome web experiences.
+        With production level experience and drive for clean and efficient solutions,
+        I love turning complex problems into elegant applications.
       </p>
       <a href="mailto:gautam.puranik95@gmail.com">
         <button>Get in Touch</button>
