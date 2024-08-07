bd8ca85b2c3d4e8f024cd432be22c5dba8d41dba
Gautam Puranik <gautam.puranik95@gmail.com>
2024-08-07 18:23:35 +0530

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -12,28 +12,22 @@ const About = (props) => {
         <h2>About me</h2>
         <div>
           <p>
-            Hey there! I'm Gautam, a Pune (India)-based FullStack developer
-            who's been on an epic coding adventure for the past almost 6 years.
+            Hey there! I'm Gautam, a Pune (India)-based JS developer
+            who's been on the coding adventure for the past 7 years.
           </p>
           <p>
-            Blending a love for user interfaces with a curious and learning
-            mindset, I'm all about crafting efficient, clean, and DRY code while
-            also keeping things simple.
+           With a strong passion for user interface design and a commitment to continuous learning,
+           I focus on developing efficient, clean, and DRY (Don't Repeat Yourself) code 
+           while prioritizing simplicity in my work.
           </p>
           <p>
             When I'm not busy coding, you'll find me exploring new destinations,
             documenting my travel escapades on my budding travel page. Water is
-            my happy place, so I'm diving into the goal of becoming a certified
-            open water diver this year!
+            my happy place and I've recently acquired the PADI certification for Open water Scuba diving.
           </p>
           <p>
-            I find joy in exploring Mythology, practicing Yoga, Dancing, playing
-            Cricket, Swimming, and indulging in Reading.
-          </p>
-          <p>
-            Additionally, I also have a keen interest in languages. Apart from
-            the 3 local languages, I have a beginner's grasp of Spanish and
-            current pursuit of learning German.
+            I find joy in exploring astrophysics, mythology, practicing yoga, dancing, playing
+            guitar, swimming, and reading.
           </p>
         </div>
       </div>
